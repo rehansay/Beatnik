8b77fd27f69d2710a606b88bcf694bf1de90039e
rehansay <sayyadrehan.787@gmail.com>
2026-07-09 13:14:36 +0530

    created sidebar

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./sidebar.css"
+import SidebarButton from './sidebarButton'
+import { MdSpaceDashboard } from "react-icons/md";
+import { FaGripfire } from "react-icons/fa";
+import { FaPlay } from "react-icons/fa";
+import { MdFavorite } from "react-icons/md";
+import { IoLibrary } from "react-icons/io5";
+import { FaSignOutAlt } from "react-icons/fa";
+
+
+function Sidebar() {
+  return (
+    <div className='sidebar-container'>
+        <img src="https://images.pexels.com/photos/10676939/pexels-photo-10676939.jpeg" 
+        className="profile-img" 
+        alt="profile"
+         />
+
+        <div>
+            <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />}/>
+            <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />}/>
+            <SidebarButton title="Player" to="/player" icon={<FaPlay />}/>
+            <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite />}/>
+            <SidebarButton title="Library" to="/" icon={<IoLibrary />}/>
+            
+
+        </div>
+        <SidebarButton title="" to="" icon={<FaSignOutAlt/>} />
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/screens/home/home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Library from '../library/library';
+import Feed from '../feed/feed';
+import Player from '../player/player';
+import Trending from '../trending/trending';
+import Favorites from '../favorites/favorites';
+import './home.css'
+import Sidebar from '../../components/sidebar';
+
+
+function Home() {
+  return (
+    <Router>
+        <div className='main-body'>
+        <Sidebar />
+
+        <Routes>
+            <Route path="/" element={<Library/>}/>
+            <Route path="/feed" element={<Feed/>}/>
+            <Route path="/player" element={<Player/>}/>
+            <Route path="/trending" element={<Trending/>}/>
+            <Route path="/favorites" element={<Favorites/>}/>
+        </Routes>
+
+        </div>
+    </Router>
+  )
+}
+
+export default Home
